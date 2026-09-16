0d1369ef9ae74489839fd0445cf08fbea0f8a013
QA-Chard <richard.sasis@eg-software.com>
2026-09-16 16:12:29 +0800

    Start of API testing

---
+++ tests/api/post.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test('API-001 Get a single post', async ({ request }) => {
+
+    // Send GET request
+
+    // Parse response body
+
+    // Add assertions
+
+});
